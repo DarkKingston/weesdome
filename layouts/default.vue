4fbe8fb2d618fd33043710ca72a68e247e19c1c9
Palii Dorin <palii.dorin4500@gmail.com>
2024-07-09 22:50:11 +0300

    add layout and configure some modules&nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {
+  setup() {
+    console.log(6556);
+    return {};
+  },
+};
+</script>
+
+<template>
+  <section>
+    <AppHeader />
+    <slot></slot>
+  </section>
+</template>
+
+<style lang="sass"></style>
