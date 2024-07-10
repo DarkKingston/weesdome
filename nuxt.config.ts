6e8df3ac256df9f0a95a5be07e7fb6a01e18bf03
Palii Dorin <palii.dorin4500@gmail.com>
2024-07-10 20:50:08 +0300

    update nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  server: {
+    host: "0.0.0.0",
+  },
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  css: ['~/assets/css/style.css'],
-  modules: ['@nuxt/image', "@nuxtjs/google-fonts"]
-})
+  css: ["~/assets/css/style.css"],
+  modules: ["@nuxt/image", "@nuxtjs/google-fonts"],
+});
