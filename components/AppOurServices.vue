7f50227fb0f6c80656d76d103760b04d2003f7e0
Apple <apple@Apples-MacBook-Pro.local>
2024-07-31 22:15:23 +0300

    add structure for service swiper

--- components/AppOurServices.vue
+++ components/AppOurServices.vue
@@ -22,7 +22,33 @@ export default {
       <div class="popular_title fz-20 fw-700 mb-0">Наши услуги</div>
       <div class="subtitle fz-16 fw-400">Чем мы можем быть вам полезны</div>
       <div class="no_overflow">
+        <div class="services_swiper">
+          <div class="swiper-wrapper">
 
+            <div class="swiper-slide">
+              <div class="services_slide_wrapper">
+
+                <div class="service_slide_title">
+
+                </div>
+
+                <div class="service_slide_subtitle">
+
+                </div>
+
+                <div class="service_slide_img">
+                  <NuxtImg src="/images/service_1.png" />
+                </div>
+
+                <div class="service_slide_btn">
+
+                </div>
+
+              </div>
+            </div>
+
+          </div>
+        </div>
       </div>
     </div>
   </section>
