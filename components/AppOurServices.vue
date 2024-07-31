50ef29440dbcdf3f033d2901c6916a9670173e5b
Apple <apple@Apples-MacBook-Pro.local>
2024-07-31 22:45:44 +0300

    add styled and info for service items

--- components/AppOurServices.vue
+++ components/AppOurServices.vue
@@ -9,7 +9,34 @@ export default {
   setup() {
 
     onMounted(() => {
-
+      new Swiper(".services_swiper", {
+        direction: "horizontal",
+        slidesPerView: 3,
+        spaceBetween: 12,
+        breakpoints: {
+          1040:{
+            slidesPerView: 3,
+          },
+          815:{
+            slidesPerView: 2.4,
+          },
+          700:{
+            slidesPerView: 2.1,
+          },
+          580:{
+            slidesPerView: 1.9,
+          },
+          445:{
+            slidesPerView: 1.4,
+          },
+          400:{
+            slidesPerView: 1.2
+          },
+          230:{
+            slidesPerView: 1.1
+          }
+        }
+      });
     });
     return {  };
   },
@@ -26,22 +53,66 @@ export default {
           <div class="swiper-wrapper">
 
             <div class="swiper-slide">
-              <div class="services_slide_wrapper">
+              <div class="services_slide_wrapper services_active">
 
-                <div class="service_slide_title">
-
+                <div class="service_slide_title fz-20 fw-700">
+                  Подбор апартаментов
                 </div>
 
-                <div class="service_slide_subtitle">
-
+                <div class="service_slide_subtitle fz-15 fw-500">
+                  Наш 15 летний опыт гарантирует лучший подбор аппартаментов в Тайланде
                 </div>
 
                 <div class="service_slide_img">
-                  <NuxtImg src="/images/service_1.png" />
+                  <NuxtImg src="/images/services.png" />
                 </div>
 
-                <div class="service_slide_btn">
+                <div class="service_slide_btn fz-16 fw-600">
+                  Получить консультацию
+                </div>
 
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="services_slide_wrapper">
+
+                <div class="service_slide_title fz-20 fw-700">
+                  Подбор апартаментов
+                </div>
+
+                <div class="service_slide_subtitle fz-15 fw-500">
+                  Наш 15 летний опыт гарантирует лучший подбор аппартаментов в Тайланде
+                </div>
+
+                <div class="service_slide_img">
+                  <NuxtImg src="/images/services.png" />
+                </div>
+
+                <div class="service_slide_btn fz-16 fw-600">
+                  Получить консультацию
+                </div>
+
+              </div>
+            </div>
+
+            <div class="swiper-slide">
+              <div class="services_slide_wrapper">
+
+                <div class="service_slide_title fz-20 fw-700">
+                  Подбор апартаментов
+                </div>
+
+                <div class="service_slide_subtitle fz-15 fw-500">
+                  Наш 15 летний опыт гарантирует лучший подбор аппартаментов в Тайланде
+                </div>
+
+                <div class="service_slide_img">
+                  <NuxtImg src="/images/services.png" />
+                </div>
+
+                <div class="service_slide_btn fz-16 fw-600">
+                  Получить консультацию
                 </div>
 
               </div>
