12062430da1ba77d2c0382b773f83f8f148dca29
Palii Dorin <palii.dorin@yandex.ru>
2024-08-07 22:20:26 +0300

    restyled footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -7,10 +7,14 @@ export default {};
     <div class="container">
       <div class="footer_wrapper">
         <NuxtLink to="/" class="footer_logo">
-          <NuxtImg src="/logo.svg" alt="" />
+          <NuxtImg src="/images/footer_logo.svg" alt="" />
         </NuxtLink>
-        <div class="footer_contact">
-          <NuxtLink to="tel:022108123" class="fz-16"> 022108123 </NuxtLink>
+        <div class="footer_subtitle fz-16 fw-300">
+          Live Your Dream
+        </div>
+        <div class="footer_contact fz-16 fw-400 tac">
+          44/34 Moo 1, Thep Krassatri Rd., Si Sunthon
+          Thalang Distrikt, Phuket 83110
         </div>
       </div>
     </div>
